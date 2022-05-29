7f72976d965449187049a6dd4e0cd3a191063a65
Rune Petersen <runembpetersen@gmail.com>
2022-05-29 20:36:57 +0200

    Changes to structure, including a much needed .gitignore file

--- MinimalApi/GlobalUsings.cs
+++ MinimalApi/GlobalUsings.cs
@@ -1,4 +1,9 @@
 ﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+
 global using MinimalApi.Models;
 global using MinimalApi.Repositories;
 global using MinimalApi.Interfaces;
