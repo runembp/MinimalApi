ded83aeef2d16dc12a0db9f6ad273cd4d992511c
Rune Petersen <runembpetersen@gmail.com>
2022-05-29 20:38:59 +0200

    Added missing global using

--- MinimalApi/GlobalUsings.cs
+++ MinimalApi/GlobalUsings.cs
@@ -3,6 +3,7 @@ global using System.Collections.Generic;
 global using System.Linq;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.DependencyInjection;
 
 global using MinimalApi.Models;
 global using MinimalApi.Repositories;
